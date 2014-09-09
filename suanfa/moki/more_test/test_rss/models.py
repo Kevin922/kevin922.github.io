0a8375ae3c6588bc78415a081409f48e8a3425be
k9 <admin1@kkk9.local>
2014-09-09 15:01:26 +0800

    在genre，唱片分类上，加索引，以实现依照‘流派’分类

--- suanfa/moki/more_test/test_rss/models.py
+++ suanfa/moki/more_test/test_rss/models.py
@@ -8,7 +8,7 @@ class RssContent(models.Model):
     album = models.CharField(max_length=50, default='', null=True) # 唱片名字
     albumLink = models.CharField(max_length=200, default='', null=True) # 唱片链接
     albumPrice = models.CharField(max_length=20, default='', null=True) # 唱片价格
-    genre = models.CharField(max_length=50, default='', null=True) # 唱片流派
+    genre = models.CharField(max_length=50, default='', null=True, db_index=True) # 唱片流派
     coverArt = models.CharField(max_length=300, default='', null=True) # 唱片封面图片链接
     releasedate = models.CharField(max_length=50, default='', null=True) # 唱片发行日期
 
